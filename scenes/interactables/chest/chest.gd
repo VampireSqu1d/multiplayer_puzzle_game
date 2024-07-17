9ac355a8f40f92e29d415d901c6aceb8a27644cb
unknown <cuauhtemoc.ayala@proton.me>
2024-07-16 21:28:30 -0700

    added key interactable object

--- scenes/interactables/chest/chest.gd
+++ scenes/interactables/chest/chest.gd
@@ -3,6 +3,9 @@ extends Node2D
 
 @onready var chest_locked: Sprite2D = $ChestLocked
 @onready var chest_unlocked: Sprite2D = $ChestUnlocked
+@export var key_scene: PackedScene
+@onready var key_spawn: Node2D = $KeySpawn
+
 @export var is_locked: = true
 
 
@@ -11,6 +14,8 @@ extends Node2D
 func _on_interactible_interacted():
 	if is_locked:
 		is_locked = false
+		var key = key_scene.instantiate()
+		key_spawn.add_child(key)
 		set_chest_properties()
 
 
