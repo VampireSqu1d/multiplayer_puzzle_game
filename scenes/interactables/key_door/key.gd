c3f136fb2866ec77b9040426cba68df9291a56b0
unknown <cuauhtemoc.ayala@proton.me>
2024-07-19 20:51:47 -0700

    added bridge component

--- scenes/interactables/key_door/key.gd
+++ scenes/interactables/key_door/key.gd
@@ -5,6 +5,8 @@ extends Node2D
 var following_body
 
 func _process(delta: float) -> void:
+	if multiplayer.multiplayer_peer == null:
+		return
 	if multiplayer.is_server():
 		if following_body != null:
 			global_position = lerp(following_body.global_position + follow_offset, global_position, pow(0.5, delta * lerp_speed))
