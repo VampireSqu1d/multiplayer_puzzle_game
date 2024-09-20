6b42106ab5344e3fdd461494ff7b9810e57b6186
VampireSqu1d <cuauhtemocayala99@gmail.com>
2024-09-19 22:29:43 -0700

    added W4Games Cloud support for web outside LAN

--- scripts/lobby.gd
+++ scripts/lobby.gd
@@ -1,8 +1,10 @@
 extends Node
 
 signal player_connected(peer_id, player_info)
+@warning_ignore("unused_signal")
 signal player_disconnected(peer_id)
 signal server_disconnected
+@warning_ignore("unused_signal")
 signal connection_failed
 
 const PORT = 7000
@@ -11,32 +13,41 @@ const MAX_CONNECTION = 3
 var players = {}
 var player_info = {"name": "player 1"}
 
+var uuid = W4Utils.UUIDGenerator.new()
 
 func _ready() -> void:
-	multiplayer.peer_connected.connect(_on_player_connected)
-	multiplayer.peer_disconnected.connect(_on_peer_disconnected)
-	multiplayer.server_disconnected.connect(_on_connection_failed)
-	multiplayer.connection_failed.connect(_on_server_disconnected)
+	#multiplayer.peer_connected.connect(_on_player_connected)
+	#multiplayer.peer_disconnected.connect(_on_peer_disconnected)
+	#multiplayer.server_disconnected.connect(_on_connection_failed)
+	#multiplayer.connection_failed.connect(_on_server_disconnected)
+	
+	LobbyHelper.lobby_created.connect(_on_lobby_created)
+	LobbyHelper.lobby_joined.connect(_on_lobby_joined)
+	
+	var login_result = await W4GD.auth.login_device(uuid.generate_v4(), uuid.generate_v4()).async()
+	
+	if login_result.is_error():
+		print(login_result.as_error().message)
+	else:
+		var user_info = await W4GD.auth.get_user().async()
+		
+		print("logged in as: ", user_info.email)
+
 
 func create_game():
-	var peer =  ENetMultiplayerPeer.new()
-	var error = peer.create_server(PORT, MAX_CONNECTION)
+	await LobbyHelper.create_lobby(LobbyHelper.LobbyType.WEBRTC)
 	
-	if error: 
-		return error
-	
-	multiplayer.multiplayer_peer = peer
-	
-	players[1] = player_info
-	player_connected.emit(1, player_info)
+	#players[1] = player_info
+	#player_connected.emit(1, player_info)
 
 
 func join_game(address):
-	var peer = ENetMultiplayerPeer.new()
-	var error = peer.create_client(address, PORT)
-	if error:
-		return error
-	multiplayer.multiplayer_peer = peer
+	await LobbyHelper.join_lobby(address)
+	#var peer = ENetMultiplayerPeer.new()
+	#var error = peer.create_client(address, PORT)
+	#if error:
+		#return error
+	#multiplayer.multiplayer_peer = peer
 
 
 func _on_player_connected(id):
@@ -72,3 +83,15 @@ func _on_server_disconnected():
 	pass
 
 
+func _on_lobby_created(lobby):
+	print("Created Lobby Id: ",lobby.id)
+	DisplayServer.clipboard_set(lobby.id)
+	lobby.player_joined.connect(_on_player_joined)
+
+
+func _on_player_joined(id):
+	print("player joined id:", id)
+
+
+func _on_lobby_joined(lobby):
+	print("joined lobby id", lobby.id)
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -6,30 +6,38 @@ extends Node
 @onready var not_connected_h_box: HBoxContainer = %NotConnectedHBox
 @onready var host_hbox: HBoxContainer = %HostHbox
 @onready var ui: Control = %UI
+@onready var leave_ui: Control = %LeaveUI
 
 @export var level_scene: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	multiplayer.connection_failed.connect(_on_connection_failed)
-	multiplayer.connected_to_server.connect(_on_connected_to_server)
+	#multiplayer.connection_failed.connect(_on_connection_failed)
+	#multiplayer.connected_to_server.connect(_on_connected_to_server)
+	
+	multiplayer.peer_disconnected.connect(_on_lobby_left)
+	LobbyHelper.lobby_webrtc_peers_ready.connect(_on_peers_ready)
+	
+	leave_ui.hide()
 
 
 func _on_host_button_pressed() -> void:
 	not_connected_h_box.hide()
 	host_hbox.show()
-	Lobby.create_game()
+	MyLobby.create_game()
 	status_label.text = "Hosting!"
 
 
 func _on_start_button_pressed() -> void:
-	hide_menu.rpc()
-	change_level.call_deferred(level_scene)
+	LobbyHelper.lobby.state = LobbyHelper.LobbyState.SEALED
+	LobbyHelper.lobby.save().async()
+	#hide_menu.rpc()
+	#change_level.call_deferred(level_scene)
 
 
 func _on_join_button_pressed() -> void:
 	not_connected_h_box.hide()
-	Lobby.join_game(ip_line_edit.text)
+	MyLobby.join_game(ip_line_edit.text)
 	status_label.text = "Connecting..."
 
 
@@ -54,9 +62,38 @@ func _on_connected_to_server():
 @rpc("call_local", "authority", "reliable")
 func hide_menu():
 	ui.hide()
+	leave_ui.show()
 
 
 func _on_level_completed():
 	call_deferred("change_level", level_scene)
 
 
+func _on_peers_ready():
+	if is_multiplayer_authority():
+		hide_menu.rpc()
+		change_level.call_deferred(level_scene)
+
+
+func _on_leave_button_pressed() -> void:
+	if is_multiplayer_authority():
+		LobbyHelper.lobby.state = LobbyHelper.LobbyState.DONE
+		LobbyHelper.lobby.save().async()
+		for c in level_holder.get_children():
+			level_holder.remove_child(c)
+			c.level_completed.disconnect(_on_level_completed)
+			c.queue_free()
+	LobbyHelper._set_lobby(null)
+	multiplayer.multiplayer_peer = null
+	return_to_menu()
+
+func _on_lobby_left(id):
+	if id == 1:
+		return_to_menu()
+
+func return_to_menu():
+	ui.show()
+	not_connected_h_box.show()
+	leave_ui.hide()
+	host_hbox.hide()
+	status_label.text = ""
